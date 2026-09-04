4d1a1bdd7fcc816ee209cb0607739762f09d695c
Auvro123 <nsk.auvro@gmail.com>
2026-09-04 20:58:55 +0300

    done

---
+++ list_comprihension.py
@@ -0,0 +1,5 @@
+li=[33,183,142,99,11,14,21]
+new_li=[]
+for x in li:
+    new_li.append(2*x)
+print(new_li)
---
+++ tuple.py
@@ -0,0 +1,7 @@
+x=(5, 10, 15, 20, 25)
+type(x)
+print(type(x))
+tpl=[1,2,3,4,5]
+
+tpl=tuple(tpl)
+print(tpl)
